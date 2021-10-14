b773f3a8bb3caa53ab2ffcdb4693bf639c3ed86c
Jessie Woods <jessieallyn@gmail.com>
2021-10-14 12:46:56 -0500

    Create woods-family.component.ts

---
+++ src/app/woods-family/woods-family.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-woods-family',
+  templateUrl: './woods-family.component.html',
+  styleUrls: ['./woods-family.component.css']
+})
+export class WoodsFamilyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
