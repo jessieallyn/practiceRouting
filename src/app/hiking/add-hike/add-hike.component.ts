9523eb29e07e4e8b679ce903027b7e46a4e0c2a1
Jessie Woods <jessieallyn@gmail.com>
2021-10-14 12:45:13 -0500

    Create add-hike.component.ts

---
+++ src/app/hiking/add-hike/add-hike.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hike',
+  templateUrl: './add-hike.component.html',
+  styleUrls: ['./add-hike.component.css']
+})
+export class AddHikeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
