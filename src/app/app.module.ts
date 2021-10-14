d2fbc9e2e915ae32ea1a3e4984424285b6e83f72
Jessie Woods <jessieallyn@gmail.com>
2021-10-14 12:45:20 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HikingComponent } from './hiking/hiking.component';
+import { WoodsFamilyComponent } from './woods-family/woods-family.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FutureComponent } from './hiking/future/future.component';
+import { PastComponent } from './hiking/past/past.component';
+import { AddHikeComponent } from './hiking/add-hike/add-hike.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WoodsFamilyComponent,
+    HikingComponent,
+    HomeComponent,
+    NavigationComponent,
+    FutureComponent,
+    PastComponent,
+    AddHikeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
