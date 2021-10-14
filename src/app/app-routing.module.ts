1bd2d510b1c2311b865cbbd24ff4f73d2263ad0e
Jessie Woods <jessieallyn@gmail.com>
2021-10-14 12:45:27 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddHikeComponent } from './hiking/add-hike/add-hike.component';
+import { FutureComponent } from './hiking/future/future.component';
+import { HikingComponent } from './hiking/hiking.component';
+import { PastComponent } from './hiking/past/past.component';
+import { HomeComponent } from './home/home.component';
+import { WoodsFamilyComponent } from './woods-family/woods-family.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'hiking', component: HikingComponent, children: [
+    {path: 'past', component: PastComponent},
+    {path: 'future', component: FutureComponent},
+    {path: 'add-hike', component: AddHikeComponent}
+  ] },
+  { path: 'woods-family', component: WoodsFamilyComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
